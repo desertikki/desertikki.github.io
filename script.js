ccf5f37ba95d3a097f1a9fce5326ab77522156dc
12345 <80477236+desertikki@users.noreply.github.com>
2026-01-13 13:51:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const scriptText = "loadstring(game:HttpGet("https://raw.githubusercontent.com/desertikki/script/refs/heads/main/script"))()"))()";
+const scriptText = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/desertikki/script/refs/heads/main/script"))()';
 
 function copyScript() {
     navigator.clipboard.writeText(scriptText);
